e459759a8a72a71678881791e49acc917a60e46c
NovanRmdhn <94606527+MochNovanRomadhon@users.noreply.github.com>
2023-11-29 07:36:35 +0700

    Update index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 5f79d573a0f7803c2ac27f24f3cef2adbb20a94c
 /* eslint-disable import/no-extraneous-dependencies */
 import '../styles/main.css';
 import 'regenerator-runtime';
@@ -22,9 +18,3 @@ window.addEventListener('load', () => {
   app.renderPage();
   swRegister();
 });
-<<<<<<< HEAD
-=======
-=======
-import '../styles/main.css';
->>>>>>> ed65d109a21df8428fedab383578726d2345b5df
->>>>>>> 5f79d573a0f7803c2ac27f24f3cef2adbb20a94c
