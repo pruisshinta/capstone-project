94b36351f6107d99c7049b86965aee1768c96f08
NovanRmdhn <94606527+MochNovanRomadhon@users.noreply.github.com>
2023-12-18 05:06:43 +0700

    Update final Firebase.js

--- src/scripts/firebase/Firebase.js
+++ src/scripts/firebase/Firebase.js
@@ -1,18 +1,14 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDb3IFwaW6dL_uNpS8ZLZySDbEELFVc1YE",
-  authDomain: "capstone-project-de553.firebaseapp.com",
-  projectId: "capstone-project-de553",
-  storageBucket: "capstone-project-de553.appspot.com",
-  messagingSenderId: "597929631188",
-  appId: "1:597929631188:web:cc695f2972ad4a40a1ca41"
+  apiKey: 'AIzaSyDb3IFwaW6dL_uNpS8ZLZySDbEELFVc1YE',
+  authDomain: 'capstone-project-de553.firebaseapp.com',
+  projectId: 'capstone-project-de553',
+  storageBucket: 'capstone-project-de553.appspot.com',
+  messagingSenderId: '597929631188',
+  appId: '1:597929631188:web:cc695f2972ad4a40a1ca41',
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Export the initialized Firebase app
 export default app;
